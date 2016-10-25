6447c4c9b38f171d3616becdc6f868dc3428df43
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2016-10-25 12:58:56 +0530

    bleh

--- cjsite/frontendapp/models.py
+++ cjsite/frontendapp/models.py
@@ -4,31 +4,31 @@ class Accesslog(models.Model):
     user_addr = models.CharField(max_length=50)
     acs_date = models.DateTimeField('date accessed')
     def __str__(self):
-    	return self.user_addr
+        return self.user_addr
 
 class User(models.Model):
-	name = models.CharField(max_length=40)
-	email = models.EmailField(max_length=254)
-	reg_date = models.DateTimeField('date registered',auto_now=True)
-	bdate = models.DateTimeField('bdate')
-	location = models.CharField(max_length=30)
-	field = models.CharField(max_length=30)
-	def __str__(self):
-		return self.email
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    reg_date = models.DateTimeField('date registered',auto_now=True)
+    bdate = models.DateTimeField('bdate')
+    location = models.CharField(max_length=30)
+    field = models.CharField(max_length=30)
+    def __str__(self):
+        return self.email
 
 class University(models.Model):
-	name = models.CharField(max_length=150, unique = True)
-	date_founded = models.DateField('date founded')
-	country = models.CharField(max_length=50)
-	website = models.URLField(default=None)
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=150, unique = True)
+    date_founded = models.DateField('date founded')
+    country = models.CharField(max_length=50)
+    website = models.URLField(default=None)
+    def __str__(self):
+        return self.name
 
 class College(models.Model):
-	university = models.ForeignKey('University', on_delete=models.CASCADE , default = None , blank = True)
-	name = models.CharField(max_length=150, unique=True)
-	address = models.CharField(max_length=300)
-	website = models.URLField()
-	date_founded = models.DateField('date founded')
-	def __str__(self):
-		return self.name
+    university = models.ForeignKey('University', on_delete=models.CASCADE , default = None , blank = True)
+    name = models.CharField(max_length=150, unique=True)
+    address = models.CharField(max_length=300)
+    website = models.URLField()
+    date_founded = models.DateField('date founded')
+    def __str__(self):
+        return self.name
